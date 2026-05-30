524c6dab229af1a3deb23319c304a3e6f4e4dd6b
yourprateek <prateekg.dev@gmail.com>
2026-05-30 12:10:10 +0530

    completed counter program

---
+++ Pages/Projects/Counter/counter.js
@@ -0,0 +1,20 @@
+const decreaseBtn = document.getElementById("decrease");
+const resetBtn = document.getElementById("reset");
+const increaseBtn = document.getElementById("increase");
+const countlabel = document.getElementById("countlabel");
+let count = 0;
+
+increaseBtn.onclick = function(){
+    count++;
+    countlabel.textContent = count;
+}
+
+decreaseBtn.onclick = function(){
+    count--;
+    countlabel.textContent = count;
+}
+
+resetBtn.onclick = function(){
+    count =0;
+    countlabel.textContent = count;
+}
